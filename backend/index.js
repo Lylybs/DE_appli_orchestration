22fb48f2861240541c7fb44c92d51e25aa69afbe
Lylybs <71543467+Lylybs@users.noreply.github.com>
2022-02-09 17:29:22 +0100

    backend update

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,32 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const session = require('express-session');
+    const MongoDBStore = require('connect-mongodb-session')(session);
+    const store = new MongoDBStore({
+        uri: "mongodb+srv://lylybs:<password>@cluster0.hppn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        collection: 'sessions'
+    });
+    app.use(
+        session({
+            secret: 'secret string',
+            resave: false,
+            saveUninitialized: false,
+            store: store,
+            cookie:{}
+        })
+    );
+
+app.get('/', function(req, res){
+    if(!req.session.counter)
+        res.session.counter = 0;
+    req.session.counter++;
+    res.send({
+        pageCount: req.session.counter
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
